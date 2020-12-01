ac17baf270e8eea9b1bfe11159b09a444026269e
Igor Lapinski <ilapinski@outlook.de>
2020-12-01 17:48:19 +0100

    Exercise 4

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 export class HomePage {
   public allBeakers =
   [
-    [1, 3, 2, 1],
+    [1, 3, 3, 1],
     [1, 3, 2, 2],
     [3, 2, 1, 3],
     [0, 0, 0, 0],
@@ -18,17 +18,22 @@ export class HomePage {
     oldBeaker: 1,
     newBeaker: 2
   };
+  public gameEnded = false; // var to disable the "Make a move" button
 
   constructor() {}
 
 
 
+  /**
+   * Function for making a move.
+   * It is executed when the button is clicked.
+   */
   public makeMove() {
     const oldBeaker = this.allBeakers[this.move.oldBeaker - 1];
     const newBeaker = this.allBeakers[this.move.newBeaker - 1];
     const unitsToMove = this.getUnitLength(oldBeaker);
 
-    if (this.isMoveLegal() === true && this.move.oldBeaker !== this.move.newBeaker) {
+    if (this.isMoveLegal(this.move.oldBeaker, this.move.newBeaker) === true && this.move.oldBeaker !== this.move.newBeaker) {
       for (let i = unitsToMove; i > 0; i--) {
         const colorOfUnitToMove = this.findLastNonZeroElement(oldBeaker);
         const indexOfOldUnit = this.getUnitLength(oldBeaker);
@@ -37,17 +42,22 @@ export class HomePage {
         this.allBeakers[this.move.newBeaker - 1][indexOfNewUnit - 1] = colorOfUnitToMove;
       }
     }
+
+    this.checkIfGameEnds();
   }
 
-
-
-  isMoveLegal() {
+  /**
+   * Checks if the move is possible.
+   * @param moveOldBeaker: old beaker, 1-5
+   * @param moveNewBeaker: new beaker, 1-5
+   */
+  isMoveLegal(moveOldBeaker: number, moveNewBeaker: number) {
     // the target beaker has enough space for the entire liquid that is going to be poured into it
     let firstRequirement = false;
     let secondRequirement = false;
 
-    const oldBeaker = this.allBeakers[this.move.oldBeaker - 1];
-    const newBeaker = this.allBeakers[this.move.newBeaker - 1];
+    const oldBeaker = this.allBeakers[moveOldBeaker - 1];
+    const newBeaker = this.allBeakers[moveNewBeaker - 1];
 
     const oldBeakerUnitLength = this.getUnitLength(oldBeaker);
     const newBeakerUnitLength = this.getUnitLength(newBeaker);
@@ -64,12 +74,52 @@ export class HomePage {
         this.findLastNonZeroElement(newBeaker) === 0) {
       secondRequirement = true;
     }
-    console.log()
 
     return (firstRequirement && secondRequirement);
   }
 
+  /**
+   * Check whether the player has won or lost.
+   */
+  checkIfGameEnds() {
+    if (this.checkIfLegalMovesArePossible() === false) {
+      console.log("[Game over!] There are no more legal moves to make.");
+      this.gameEnded = true;
+      return;
+    }
 
+    if (this.checkForContent() === true) {
+      console.log("[You won!] Congratulations.");
+      this.gameEnded = true;
+      return;
+    }
+  }
+
+  checkIfLegalMovesArePossible() {
+    let isPossible = false;
+    for (let i = 5; i > 0; i--) {
+      for (let j = 5; j > 0; j--) {
+        isPossible = true;
+      }
+    }
+
+    return isPossible;
+  }
+
+  /**
+   * Checks if the column has homogeneous content
+   */
+  checkForContent() {
+    let hasSameContent = false;
+
+    for (let i = 0; i < this.allBeakers.length; i++) { // every beaker
+      const isBeakerHomogeneous = this.allBeakers[i].every((value, index, array) => value === array[0]); // same content?
+      if(!isBeakerHomogeneous) {
+        hasSameContent = false;
+      }
+    }
+    return hasSameContent;
+  }
 
   private findLastNonZeroElement(array: number[]) {
     let lastNonZero = 0;
